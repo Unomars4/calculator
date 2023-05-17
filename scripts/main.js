78c312bd8629263eaf9616a01dc712073dff046d
Unomars4 <une.nyide@gmail.com>
2023-05-17 17:59:35 +0200

    add starter functions and buttons

---
+++ scripts/main.js
@@ -0,0 +1,25 @@
+const display;
+const firstNum;
+const secondNum;
+const operator;
+
+
+function operate(a, b, op) {
+
+}
+
+function add(a, b) {
+    return a + b;
+}
+
+function subtract(a,b) {
+    return a -b;
+}
+
+function muliply(a, b) {
+    return a * b;
+}
+
+function divide(a, b) { 
+    return a - b;
+}
